469510cbce1e5c9a1f32bd984d48e1b916a9d607
spmohammedyusuf <spmohammedyusuf@gmail.com>
2025-09-14 20:52:46 +0530

    first commit

---
+++ Num.js
@@ -0,0 +1,7 @@
+for (let i = 1; i <= 5; i++) {
+  let line = "";
+  for (let j = 1; j <= i; j++) {
+    line = line + "1";
+  }
+  console.log(line);
+}
---
+++ phone-bills.js
@@ -0,0 +1,5 @@
+let call = 150;
+let totalbill;
+if(call <=100){
+    totalbill=0;
+}
