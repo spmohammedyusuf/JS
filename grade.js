6ef138c87a4f0c2206b672fc5fcce0f622cf63f4
spmohammedyusuf <spmohammedyusuf@gmail.com>
2025-09-20 19:44:56 +0530

    grade

---
+++ grade.js
@@ -0,0 +1,14 @@
+let percent = 45;
+let grade = "";
+
+if (percent >= 80) {
+  grade = "A";
+} else if (percent >= 60) {
+  grade = "B";
+} else if (percent >= 40) {
+  grade = "C";
+} else {
+  grade = "D";
+}
+console.log("percent = " + percent);
+console.log("grade = " + grade);
